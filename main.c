96d1850baa8985308ee099f0efc34082b9cc90ba
Nathan Gibson <gibson.nathan.2011@gmail.com>
2015-01-31 22:17:13 -0500

    queue working

--- main.c
+++ main.c
@@ -8,31 +8,68 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
 
-struct queueNode{
-    int i;
-    struct queueNode* next;
+struct widget{
+    pthread_t producersID;
+    int widgetNumber;
+    struct widget* next;
 };
 
-static struct queueNode* head;
+static struct widget* head;
 void enqueue();
-int dequeue();
+struct widget* dequeue();
 int isEmpty();
+void* producer();
+void* consumer();
 
 int main(int argc, const char * argv[]) {
+    
+    head = NULL; //queue is initially empty
 
+    //get command line arguments
+    int numberOfProducers = atoi(argv[1]);
+    int numberOfConsumers = atoi(argv[2]);
+    int numberOfWidgets = atoi(argv[3]);
+    int timeToWait = atoi(argv[4]);
+    
+    //testing queue
+    struct widget* widg1 = (struct widget*)malloc(sizeof(struct widget));
+    struct widget* widg2 = (struct widget*)malloc(sizeof(struct widget));
+    struct widget* widg3 = (struct widget*)malloc(sizeof(struct widget));
+    struct widget* widg4 = (struct widget*)malloc(sizeof(struct widget));
+    
+    widg1 -> widgetNumber = 1;
+    widg2 -> widgetNumber = 2;
+    widg3 -> widgetNumber = 3;
+    widg4 -> widgetNumber = 4;
+    
+    enqueue(widg1);
+    enqueue(widg2);
+    enqueue(widg3);
+    enqueue(widg4);
+
+    //have to make sure that head isn't NULL before
+    //trying to access its members
+    printf("%d", head ? dequeue() ->widgetNumber : -1);
+    printf("%d", head ? dequeue() ->widgetNumber : -1);
+    printf("%d", head ? dequeue() ->widgetNumber : -1);
+    printf("%d", head ? dequeue() ->widgetNumber : -1);
+    printf("%d", head ? dequeue() ->widgetNumber : -1);
+    
     return 0;
 }
 
-void enqueue(int num){
-    struct queueNode* newNode = (struct queueNode*)malloc(sizeof(struct queueNode));
+void enqueue(struct widget* widget){
+    struct widget* newNode = (struct widget*)malloc(sizeof(struct widget));
     newNode -> next = NULL;
-    newNode -> i = num;
+    newNode -> producersID = pthread_self();
+    newNode -> widgetNumber = widget -> widgetNumber;
     
     if (head == NULL) {
         head = newNode;
     }else{
-        struct queueNode* headCopy = head;
+        struct widget* headCopy = head;
         while(headCopy -> next != NULL){
             headCopy = headCopy->next;
         }
@@ -40,17 +77,21 @@ void enqueue(int num){
     }
 }
 
-int dequeue(){
-    int returnValue = -1;
+struct widget* dequeue(){
+    struct widget* returnValue = (struct widget*)malloc(sizeof(struct widget));
     if (head == NULL) {
-        return returnValue;
+        return NULL;
     }
     else if(head -> next == NULL){
-        returnValue = head -> i;
+        returnValue -> producersID = head -> producersID;
+        returnValue -> widgetNumber = head -> widgetNumber;
+        returnValue -> next = head -> next;
         head = NULL;
     }
     else{
-        returnValue = head -> i;
+        returnValue -> producersID = head -> producersID;
+        returnValue -> widgetNumber = head -> widgetNumber;
+        returnValue -> next = head -> next;
         head = head -> next;
     }
     return returnValue;
